6a8fa76804030cb116f9b079d055889e66e6bded
eit-jetson-xavier <ukabhijit@gmail.com>
2021-12-19 22:21:53 +0100

    Option feature added for bash files

--- Image_classification_nd/run_keras.sh
+++ Image_classification_nd/run_keras.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-python3 run_test_keras.py &> /dev/null
-
-input="logs_keras.log"
-while IFS= read -r line
-do
-  echo "$line"
-done < "$input"
+option=$1
+if [[ $option == "-debug" ]]
+then 
+  python3 run_test_keras.py 
+  input="logs_keras.log"
+  while IFS= read -r line
+  do
+    echo "$line"
+  done < "$input"
+elif [[ $option == "-no-debug" ]]
+then
+  python3 run_test_keras.py &> /dev/null
+  input="logs_keras.log"
+  while IFS= read -r line
+  do
+    echo "$line"
+  done < "$input"
+else
+  echo "Please provide no-debug or debug option"
+fi
